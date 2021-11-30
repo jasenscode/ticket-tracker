fe9931fbf901d0e2f050375082ac894d1bf651d4
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-11-30 18:40:16 +0000

    corrected incorrect class names in the counter sass file

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CardContainer>
-        <EmployeeInfo />
-        <Counter />
-      </CardContainer>
-
-      <div className="card">
-        <div className="card__employee">
-          <h2 className="card__info">NAME</h2>
-          <p className="card__detail">Jasen Chawda </p>
-          <h2 className="card__info">ROLE</h2>
-          <p className="card__detail">Junior Developer</p>
-        </div>
-        <div className="card__counter">
-          <h3 className="card__number">0</h3>
-          <div className="card__controls">
-            <button className="card__btn">
-              <span className="fas fa-plus"></span>
-            </button>
-            <p className="card__title">Counter</p>
-            <button className="card__btn">
-              <span className="fas fa-minus"></span>
-            </button>
-          </div>
-        </div>
-      </div>
+      <CardContainer />
+      <Card />
+      <EmployeeInfo />
+      <Counter />
     </div>
   );
 }
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -4,12 +4,12 @@ const Counter = () => {
   return (
     <div className="card__counter">
       <h3 className="card__number">0</h3>
-      <div className="card__control">
-        <button className="card__increment">
+      <div className="card__controls">
+        <button className="card__increment card__btn">
           <span className="fas fa-plus change"></span>
         </button>
         <p className="card__title">Counter</p>
-        <button className="card__decrement">
+        <button className="card__decrement card__btn">
           <span className="fas fa-minus change"></span>
         </button>
       </div>
