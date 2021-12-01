70fd87e86f381237cb700211498edc8a6e242a45
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-12-01 16:37:22 +0000

    Added counter functionality

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,15 +1,28 @@
 import "./Counter.scss";
+import { useState } from "react";
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div className="counter">
-      <span className="counter__number">0</span>
+      <span className="counter__number">{count}</span>
       <div className="counter__controls">
-        <button className="counter__increment counter__btn">
+        <button onClick={decrementCount} className="counter__decrement counter__btn">
           <span className="fas fa-minus change"></span>
         </button>
         <p className="counter__title">Counter</p>
-        <button className="counter__decrement counter__btn">
+        <button onClick={incrementCount} className="counter__increment counter__btn">
           <span className="fas fa-plus change"></span>
         </button>
       </div>
